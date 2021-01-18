006fa23799100901f285908823eecc9929835f4f
Zarrar Malik <zarrarmalik@coop.co.uk>
2021-01-18 10:34:10 +0000

    Add all dependencies

---
+++ src/main/java/com/zarrar/microservices/msccbreweryclient/MsccBreweryClientApplication.java
@@ -0,0 +1,13 @@
+package com.zarrar.microservices.msccbreweryclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsccBreweryClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsccBreweryClientApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zarrar/microservices/msccbreweryclient/MsccBreweryClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zarrar.microservices.msccbreweryclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsccBreweryClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
